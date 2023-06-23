7cdbc16baf3ea54bae55c9e6fae942a54dd7d5c4
Kundan Tripathi <72913540+KundanTripathi@users.noreply.github.com>
2023-06-24 02:44:54 +0530

    Add files via upload

--- test2.py
+++ test2.py
@@ -16,37 +16,40 @@ Chromosome_length = 6
 
 class Individual:
     
-    def __init__(self):
-        random.shuffle(Solution_sequence1)
-        self.gene1 = Solution_sequence1
-        random.shuffle(Solution_sequence2)
-        self.gene2 = Solution_sequence2
         
-
+    def __init__(self):
+        
+        random.shuffle(Solution_sequence1)
+        random.shuffle(Solution_sequence2)
+        
+        self.gene = [Solution_sequence1,Solution_sequence2]
+        
         
     # calculate fitness value by increasing fitness value by when it matches any digit
     def get_fitness(self):
-        fitness = sum(list(np.multiply(np.multiply(self.gene1,self.gene2),Cost))) + self.penalty() 
+        fitness = sum(list(np.multiply(np.multiply(self.gene[0],self.gene[1]),Cost))) + self.penalty() 
         return fitness
     
     def penalty(self):
+        self.gene = self.gene
         for i in [0,2,4]:
-            if self.gene2[i]+self.gene2[i+1] != 1 or sum(list(np.multiply(self.gene1,self.gene2))) != 60:
+            if self.gene[1][i]+self.gene[1][i+1] != 1 or sum(list(np.multiply(self.gene[0],self.gene[1]))) != 60:
                 return 1000
             else:
                 return 0
          
     def __repr__(self):
-        return ''.join(str(genes) for genes in self.gene1), ''.join(str(genes) for genes in self.gene2)
-
+        return ''.join(str(genes)+ " " for genes in np.hstack((["gene[0]"],self.gene[0],["|"],["gene[1]"],self.gene[1])))
+        
 
   
 class Population:
     #The class needs population_size as a parameter where as Individual create genes without any parameter
     def __init__(self, population_size):
         self.population_size = population_size
-        self.individuals = [Individual() for _ in range(population_size)]
-    
+        self.individuals = [Individual() for _ in range(self.population_size)]
+
+       
     #gives fittest individual from list of (population_size) individuals [self.individuals = population]   
     def get_fittest(self):
         
@@ -66,20 +69,24 @@ class Population:
     def save_individual(self, index, individual):
         self.individuals[index] = individual
         
+        
 class GeneticAlgorithm:
         
-    def __init__(self, population_size=100, crossover_rate = 0.65, mutation_rate = 0.1):
+    def __init__(self, population_size=200, crossover_rate = 0.65, mutation_rate = 0.4):
         self.population_size = population_size
         self.crossover_rate = crossover_rate
         self.mutation_rate = mutation_rate
         
     def run(self):
         pop = Population(self.population_size)
+        fitness_value = []
         
         
-        for generation_counter in range(20):
+        for generation_counter in range(200):
             print('Generation %s - fittest is : %s with fitness value %s' % (generation_counter, pop.get_fittest(), pop.get_fittest().get_fitness()))
             pop = self.evolve_population(pop)
+            fitness_value.append(pop.get_fittest().get_fitness())
+        print(sorted(fitness_value))
             
        # print("solution found ...") 
         print(pop.get_fittest())
@@ -89,41 +96,57 @@ class GeneticAlgorithm:
         
         # cross over may preserve the match
         #crossover - all index of next population is undergoing crossover depending on cross over prob.
+       
+       
         for index in range(next_population.get_size()):
             first = self.random_selection(population) # fittest of 20 random
-            second = self.random_selection(population) # fittest of 20 random form 100
-            next_population.save_individual(index,self.crossover(first,second)) 
-        
+      #      second = self.random_selection(population) # fittest of 20 random form 100
+       #     if first.get_fitness() < second.get_fitness():
+            next_population.save_individual(index,first)
+       #     else:
+      #          next_population.save_individual(index,second) 
+     
         # lower mutation prob preserve the sequence higher changes it    
         #mutation - population at all index are undergoing mutation
         for individual in next_population.individuals:
             self.mutate(individual)
             
         return next_population
-    
+    """ 
     def crossover(self, individual1, individual2):
         cross_individual = Individual() # single individual genes randomly generated as only one child from 2 parents
-        start = randint(Chromosome_length)
-        end = randint(Chromosome_length)
+        start1 = randint(Chromosome_length)
+        end1 = randint(Chromosome_length)
         
-        if end < start:
-            start , end = end , start
-            
-        cross_individual.gene1 = individual1.gene1[:start]+individual2.gene1[start:end]+individual1.gene1[end:]
-        cross_individual.gene2 = individual1.gene2[:start]+individual2.gene2[start:end]+individual1.gene2[end:]
+        start2 = randint(Chromosome_length)
+        end2 = randint(Chromosome_length)
+        
+        
+        if end1 < start1:
+            start1 , end1 = end1 , start1
+        cross_individual.gene[0] = individual1.gene[0][:start1]+individual2.gene[0][start1:end1]+individual1.gene[0][end1:]
+        
+        if end2 < start2:
+            start2 , end2 = end2 , start2
+        cross_individual.gene[1] = individual1.gene[1][:start2]+individual2.gene[1][start2:end2]+individual1.gene[1][end2:]
 
         return cross_individual
-        
+    """      
+    
     def mutate(self, individual):
         a = randint(Chromosome_length)
         b = randint(Chromosome_length)
         c = randint(Chromosome_length)
         
-        individual.gene1[a] = individual.gene1[b]
-        individual.gene1[b] = individual.gene1[c]
+        individual.gene[0][a], individual.gene[0][b] = individual.gene[0][b], individual.gene[0][a]
+        individual.gene[0][b], individual.gene[0][c] = individual.gene[0][c], individual.gene[0][b]
         
-        individual.gene2[a] = individual.gene2[b]
-        individual.gene2[b] = individual.gene2[c]
+        e = randint(Chromosome_length)
+        f = randint(Chromosome_length)
+        g = randint(Chromosome_length)
+        
+        individual.gene[1][e], individual.gene[1][f] = individual.gene[1][f], individual.gene[1][e]
+        individual.gene[1][f], individual.gene[1][g] = individual.gene[1][g], individual.gene[1][f]
         
     
     def  random_selection(self, actual_population):
@@ -137,7 +160,7 @@ class GeneticAlgorithm:
         return new_population.get_fittest() #gives fittest of 20 random
     
 if __name__  == '__main__':
-    print (Individual())
+    #print (GeneticAlgorithm(20).evolve_population(Population(15)))
     
-    #algorithm = GeneticAlgorithm(50)
-    #algorithm.run()
+    algorithm = GeneticAlgorithm()
+    algorithm.run()
